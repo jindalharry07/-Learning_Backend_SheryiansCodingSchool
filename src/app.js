9d11566dc85b5be44520082aca0f54fda77a2459
jindalharry07 <jindalharry07@gmail.com>
2026-02-10 15:20:23 +0530

    Created APIs

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const noteModel = require("./models/notes.model");
+
 // const notes = [];
 
 // app.post("/notes", (req, res) => {
@@ -40,5 +42,49 @@ app.use(express.json());
 //     message: "Delete"
 //   })
 // })
+
+app.post("/notes", async (req, res) => {
+  const data = req.body;
+  await noteModel.create({
+    title: data.title,
+    description: data.description,
+  });
+
+  res.status(201).json({
+    message: "Note Created!",
+  });
+});
+
+app.get("/notes", async (req, res) => {
+  const notes = await noteModel.find();
+
+  res.status(200).json({
+    message: "Notes fetched",
+    notes: notes,
+  });
+});
+
+app.delete("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await noteModel.findOneAndDelete({
+    _id: id,
+  });
+
+  res.status(200).json({
+    message: "Note Deleted!",
+  });
+});
+
+app.patch("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const description = req.body.description;
+
+  await noteModel.findOneAndUpdate({ _id: id }, { description: description });
+
+  res.status(200).json({
+    message: "Note Updated!"
+  })
+});
 
 module.exports = app;
