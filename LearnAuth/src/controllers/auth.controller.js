60e58dd590268d10c828fda042f005dde0f3cca7
jindalharry07 <jindalharry07@gmail.com>
2026-02-18 13:38:26 +0530

    Learned how to verify and decode jwt token

--- LearnAuth/src/controllers/auth.controller.js
+++ LearnAuth/src/controllers/auth.controller.js
@@ -4,16 +4,26 @@ const jwt = require('jsonwebtoken');
 async function registerUser(req, res) {
   const { username, email, password } = req.body;
 
+  const isUserExit = await userModel.findOne({email});
+
+  if(isUserExit) {
+    return res.status(409).json({
+      message: "User Exit"
+    })
+  }
+
   const user = await userModel.create({ username, email, password });
 
   const token = jwt.sign({
     id: user._id
   }, process.env.JWT_SECRET);
 
+  res.cookie("token", token);
+
   res.status(201).json({
     message: "User Registered Successfully!",
-    user,
-    token
+    user
+    // token
   });
 }
 
--- LearnAuth/src/routes/auth.routes.js
+++ LearnAuth/src/routes/auth.routes.js
@@ -4,4 +4,11 @@ const authController = require("../controllers/auth.controller");
 
 router.post("/register", authController.registerUser);
 
+// router.get('/test', (req, res) => {
+//   console.log("Cookies: ", req.cookies);
+//   res.json({
+//     message: "TestRoute"
+//   })
+// })
+
 module.exports = router;
